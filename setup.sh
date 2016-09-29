7629818b77e100d2b4925bec156aa6a5e0e62d22
tkjone <thomasmattacchione@gmail.com>
2016-09-29 14:19:01 -0400

    Automate symlinking in the setup script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,16 @@
 # setup
 # ==============================================================================
 
+
+# remove existing dotfiles
 rm -rf ~/.zsh_profile
 rm -rf ~/.zshrc
 rm -rf ~/.gitconfig
 rm -rf ~/.vimrc
 
+# symlink to dotfiles
+ln -s ~/.dotfiles/.zsh_profile     ~/.zsh_profile
+ln -s ~/.dotfiles/.zshrc           ~/.zshrc
+ln -s ~/.dotfiles/.gitconfig       ~/.gitconfig
+ln -s ~/.dotfiles/.vimrc           ~/.vimrc
 
